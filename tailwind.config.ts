22d2a21ad3e21dfbcb04cfb553a3b881f6fc366e
Jin Nakamura <jin.nakamura@nanimiru.jp>
2025-02-04 22:03:43 +0900

    Revert "feat: カラー定義"
    This reverts commit f01a6a3423843346942874a25503c685fd1336e0.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,121 +40,82 @@ const config: Config = {
             500: "#121315",
           },
           gray: {
-            DEFAULT: "#9f9f9f",
-            0: "#fff",
-            50: "#fafafa",
-            100: "#f5f5f5",
-            200: "#efefef",
-            300: "#e1e1e1",
-            400: "#bebebe",
-            500: "#9f9f9f", // disable
-            600: "#9f9f9f", // placeholder
-            700: "#626262", // text
-            800: "#434343", // text
-            900: "#222", // text
-            1000: "#000" // text
-          },
-          blueGray: {
-            DEFAULT: "#aab6c4",
-            100: "#f5f7fB",
-            200: "#edeff4",
-            300: "#d8dfe7",
-            400: "#c4cfda",
-            500: "#aab6c4", // disable
-            600: "#8a91a3", // placeholder
-            700: "#6a707f", // text
-            800: "#4e515c", // text
-            900: "#353940", // text
-            1000: "#121315" // text
+            DEFAULT: "#6a707f",
+            25: "#fafbfd",
+            50: "#f5f7fb",
+            75: "#f4f4f4",
+            100: "#edeff4",
+            200: "#d8dfe7",
+            300: "#c4cfda",
+            400: "#8a91a3",
+            500: "#6a707f",
+            600: "#4e515c",
           },
           blue: {
-            DEFAULT: "#95d4e5",
-            100: "#f0f6fd",
-            200: "#e2eef9",
-            300: "#c7e2f2",
-            400: "#acd7ea",
-            500: "#95d4e5", // disable
-            600: "#81cce1", // placeholder
-            700: "#47afd0", // text
-            800: "#2183cb", // text
-            900: "#2a66a8", // text
-            1000: "#1a4fa1" // text
+            DEFAULT: "#1a4fa1",
+            50: "#f0f6fd",
+            300: "#2183cb",
+            500: "#1a4fa1",
           },
-          red: {
-            DEFAULT: "#f89c93",
-            100: "#fcf0f0",
-            200: "#ffe5e2",
-            300: "#ffd4ce",
-            400: "#ffbfb6",
-            500: "#f89c93", // disable
-            600: "#ec7864", // placeholder
-            700: "#ea4e42", // text
-            800: "#e23030", // text
-            900: "#cc3333", // text
-            1000: "#b72929" // text
+          cyan: {
+            DEFAULT: "#00a3e0",
+            400: "#66b1ff",
+            500: "#00a3e0",
           },
           green: {
-            DEFAULT: "#71b599",
-            100: "#e7f2ed",
-            200: "#cceadc",
-            300: "#a9d8c3",
-            400: "#8bc6ab",
-            500: "#71b599", // disable
-            600: "#59a88a", // placeholder
-            700: "#3e8977", // text
-            800: "#217f67", // text
-            900: "#156b53", // text
-            1000: "#0e5942" // text
+            DEFAULT: "#0e5942",
+            50: "#f4f9f6",
+            100: "#e2f1e9",
+            300: "#38a169",
+            500: "#0e5942",
+          },
+          emerald: {
+            DEFAULT: "#45da8a",
+            500: "#45da8a",
+          },
+          rose: {
+            DEFAULT: "#f56c6c",
+            400: "#f56c6c",
+          },
+          pink: {
+            DEFAULT: "#ff1464",
+            400: "#ff1464",
+          },
+          red: {
+            DEFAULT: "#ff6154",
+            50: "#fdf3f5",
+            100: "#FFE0E0",
+            200: "#ff6154",
+            300: "#ff5252",
+            500: "#f21a11",
+            600: "#b72929",
           },
           yellow: {
-            DEFAULT: "#f4df89",
-            100: "#fffae1",
-            200: "#fcf5d9",
-            300: "#fcf0b6",
-            400: "#f9eaa0",
-            500: "#f4df89", // disable
-            600: "#e8cd58", // placeholder
-            700: "#e2c139", // text
-            800: "#d2aa00", // text
-            900: "#ad7c10", // text
-            1000: "#915e0d" // text
+            DEFAULT: "#F8D642",
+            25: "#fff5e1",
+            50: "#FFF9E5",
+            100: "#FFF3CC",
+            200: "#FFE799",
+            300: "#FFDB66",
+            400: "#FFD033",
+            500: "#F8D642",
+            600: "#CCB633",
+            700: "#998722",
+            800: "#665611",
+            900: "#332B08",
+          },
+          purple: {
+            DEFAULT: "#805ad5",
+            100: "#e9d8fd",
+            200: "#d6bcfa",
+            400: "#9f7aea",
+            500: "#805ad5",
+            600: "#682ad5",
           },
           orange: {
-            DEFAULT: "#f4df89",
-            100: "#ffeccc",
-            200: "#ffdfad",
-            300: "#fdd291",
-            400: "#ffbb63",
-            500: "#ffa037", // disable
-            600: "#f68511", // placeholder
-            700: "#e46f00", // text
-            800: "#cb5d00", // text
-            900: "#b14c00", // text
-            1000: "#953d00" // text
-          },
-          transparentBlack: {
-            100: "#0000001A",
-            200: "#00000033",
-            300: "#0000004D",
-            400: "#00000066",
-            500: "#00000080",
-            600: "#00000099",
-            700: "#000000B2",
-            800: "#000000CC",
-            900: "#000000E5",
-            1000: "#000000F2"
-          },
-          transparentWhite: {
-            100: "#ffffff1A",
-            200: "#ffffff33",
-            300: "#ffffff4D",
-            400: "#ffffff66",
-            500: "#ffffff80",
-            600: "#ffffff99",
-            700: "#ffffffB2",
-            800: "#ffffffCC",
-            900: "#ffffffE5",
-            1000: "#ffffffF2"
+            DEFAULT: "#FF8A00",
+            600: "#ea580c",
+            700: "#B94726",
           },
         },
       },
