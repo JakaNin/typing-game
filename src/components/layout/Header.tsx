a301b1ceea87dc4da7bf51625a62770bc1b11040
Jin Nakamura <jin.nakamura@nanimiru.jp>
2025-02-09 12:49:17 +0900

    add Link on Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,25 +1,32 @@
 import Typography from "components/commons/Typography";
 import Button from "components/commons/Button";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-white border-b border-b-ap-gray-100 border-t-2 w-full sticky top-0 z-50 shadow">
+    <header className="bg-ap-transparentWhite-900 border-b border-b-ap-gray-100 border-t-2 w-full sticky top-0 z-50 shadow">
       <div className="flex px-[1.625rem] flex-row h-full justify-between items-center p-4">
-        <div>
+        <Link href="/">
           <Typography element="h1" variant="title1">
             万葉タイピング
           </Typography>
-        </div>
+        </Link>
         <div className="flex items-center gap-3">
-          <Button variant="text">
-            <Typography variant="subtitle1">ランキング</Typography>
-          </Button>
-          <Button variant="text">
-            <Typography variant="subtitle1">遊び方</Typography>
-          </Button>
-          <Button variant="text">
-            <Typography variant="subtitle1">会社概要</Typography>
-          </Button>
+          <Link href="/ranking">
+            <Button variant="text">
+              <Typography variant="subtitle1">ランキング</Typography>
+            </Button>
+          </Link>
+          <Link href="/how-to-play">
+            <Button variant="text">
+              <Typography variant="subtitle1">遊び方</Typography>
+            </Button>
+          </Link>
+          <Link href="/about">
+            <Button variant="text">
+              <Typography variant="subtitle1">会社概要</Typography>
+            </Button>
+          </Link>
         </div>
       </div>
     </header>
